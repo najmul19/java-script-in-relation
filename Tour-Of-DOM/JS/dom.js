204d04c56cdddf119e8755164d5e8c97f6f56fec
najmul <mdnajmulislam10992@gmail.com>
2025-03-11 23:58:15 +0600

    explore style using javascript

--- Tour-Of-DOM/JS/dom.js
+++ Tour-Of-DOM/JS/dom.js
@@ -1,2 +1,16 @@
-console.log("hello JS")
-console.log(document)
+// console.log("hello JS")
+// console.log(document)
+const section = document.querySelectorAll('section')
+// console.log(section);
+for(const sect of section){
+    sect.style.border = '2px solid steelblue'
+    sect.style.marginBottom = '5px'
+    sect.style.borderRadius = '15px'
+    sect.style.paddingLeft='7px'
+    sect.style.backgroundColor='lightGray'
+}
+// const placesContainer = document.getElementById('places-container');
+// placesContainer.style.backgroundColor = 'yellow'
+const placesContainer = document.getElementById('places-container');
+placesContainer.classList.add('yellow-bg')
+placesContainer.classList.remove('large-text')
