482982d8053b0b0739d14a49718b5608aa07ae53
najmul <mdnajmulislam10992@gmail.com>
2025-03-13 20:12:11 +0600

    verify pin number and add money to the ballance

--- projects/payoo-mfs/js/home.js
+++ projects/payoo-mfs/js/home.js
@@ -1,7 +1,7 @@
 // document.getElementById('bnt-add-money').addEventListener('click',function(event){
 //     event.preventDefault();
-//     const valu1= document.getElementById('value')
-//     const value= document.getElementById('value').innerText;
+//     const valu1= document.getElementById('ballance')
+//     const value= document.getElementById('ballance').innerText;
 //     const ammount = document.getElementById('add-ammount').value;
 //     ammount = value+ammount;
 //     valu1.appendChild(ammount)
@@ -15,4 +15,15 @@ document.getElementById('bnt-add-money').addEventListener('click',function(event
     console.log(ammount)
     const pinNumber = document.getElementById('pin-number').value;
     console.log(pinNumber)
+    // wrong way
+    if(pinNumber==='1234'){
+        console.log('adding money to your account')
+        const ballance= document.getElementById('ballance').innerText;
+        const newBallance = parseFloat(ballance)+ parseFloat(ammount) ;
+        console.log(newBallance)
+        document.getElementById('ballance').innerText = newBallance;
+       
+    } else{
+        console.log('failded to add money! please try again')
+    }
 })
